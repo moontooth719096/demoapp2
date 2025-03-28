c82e041a613e891fe6b9843e4ba7603727e16005
KevinWu <KevinWu@wits.com>
2025-03-28 06:23:50 +0000

    新增日誌檢視器功能；更新 API 基礎 URL；新增日誌格式化工具；調整環境變數以支持日誌寫入

---
+++ src/utils/Log.ts
@@ -0,0 +1,14 @@
+import { axiosBase } from "@/utils/ApiHelper";
+import { LogLevel } from "@/types/CallLog";
+
+export async function Log(message: string, level: LogLevel): Promise<void> {
+  const apiHelp = axiosBase();
+  const postdata = {
+    Message: message,
+    LogLevel: level,
+    CallEnd: 1,
+  };
+  try {
+    await apiHelp.post(import.meta.env.VITE_LogUri, postdata);
+  } catch {}
+}
--- src/views/LogViewer/LogViewer.vue
+++ src/views/LogViewer/LogViewer.vue
@@ -1,29 +1,74 @@
 <template>
-  <div>
-    <h1>API Log Viewer</h1>
+  <div class="logview">
     <vue-good-table
+      class="logboard"
       :columns="columns"
       :rows="logs"
+      max-height="82%"
       :pagination-options="{ enabled: true, perPage: 10 }"
-    />
+    >
+      <template #table-actions>
+        <button type="button" class="btn btn-primary btn-sm" @click="fetchLogs">
+          Refresh
+        </button>
+      </template>
+    </vue-good-table>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
-import { useLogViewer } from "../../services/useLogViewer";
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+import { axiosBase } from "@/utils/ApiHelper";
+import { LogViewDetail } from "@/types/LogView/LogViewDetail"; // 新增匯入
 
-export default defineComponent({
-  name: "LogViewer",
-  setup() {
-    const { logs, columns } = useLogViewer();
-    return { logs, columns };
+const logs = ref<LogViewDetail[]>([]);
+const columns = ref([
+  {
+    label: "時間",
+    field: "Timestamp",
+    type: "date",
+    width: "15%",
+    dateInputFormat: "yyyy-MM-dd'T'HH:mm:ss", // 修正格式
+    dateOutputFormat: "yyyy-MM-dd HH:mm:ss", // outputs Mar 16th 2018
   },
-});
+  { label: "UserID", field: "UserID", width: "15%" },
+  { label: "LogLevel", field: "LogLevelString", width: "10%" },
+  { label: "Message", field: "Message", width: "50%" },
+  { label: "呼叫端", field: "CallEndString", width: "10%" },
+]);
+
+const fetchLogs = async () => {
+  let apihelper = axiosBase();
+  try {
+    const { data } = await apihelper.get<LogViewDetail[]>("/api/Log/read");
+
+    //判斷回傳是否有值
+    if (data !== null && data.length > 0) {
+      logs.value = data;
+    } else {
+      return;
+    }
+  } catch (error) {
+    console.error("Error fetching logs:", error);
+  }
+};
+
+onMounted(fetchLogs);
 </script>
 
-<style scoped>
-h1 {
-  margin-bottom: 20px;
+<style scoped lang="scss">
+.logview {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  .logboard {
+    padding: 1vw 1vh;
+    width: 100%;
+    height: 100%;
+    ::v-deep(.vgt-inner-wrap) {
+      height: 100%;
+    }
+  }
 }
 </style>
